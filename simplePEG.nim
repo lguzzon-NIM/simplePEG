12a12854ad9ec199367c24c06cf59aa9f944e8a3
Luca Guzzon <luca.guzzon@gmail.com>
2017-09-07 17:54:48 +0200

    Update simplePEG.nim

--- simplePEG.nim
+++ simplePEG.nim
@@ -24,6 +24,7 @@ type
 template original(aSimplePEGSlice: SimplePEGSliceObject): untyped = 
   aSimplePEGSlice.FSimplePEG.FSlices[aSimplePEGSlice.FSlicesIndex]
 
+
 proc invalidate(aSimplePEGSlice: SimplePEGSliceObject): SimplePEGSliceObject =
   aSimplePEGSlice.FSimplePEG.FSlices[aSimplePEGSlice.FSlicesIndex].FAsString = Nothing[string]()
   result = aSimplePEGSlice.FSimplePEG.FSlices[aSimplePEGSlice.FSlicesIndex]
